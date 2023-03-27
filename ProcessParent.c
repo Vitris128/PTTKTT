6aa5ac4b498f7ba992bc5e9b96778a19d2f8f6c8
Lê Nhật Trọng <88506046+Vitris128@users.noreply.github.com>
2023-03-27 18:51:53 +0700

    Create ProcessParent.c

---
+++ ProcessParent.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void) {
+    pid_t pid;
+    int n;
+    printf("n = ");
+    scanf("%d", &n);
+    pid = vfork();
+    int childrenPID=0;
+    if(pid<0) {
+        printf("\n Unable to create child process!\n");
+        exit(EXIT_FAILURE);
+    }
+    else if(pid ==0) {
+        childrenPID=getpid();
+        printf("PID's P2: %d\n", childrenPID);
+        int i;
+        printf("--- Multiplicationtable ---\n");
+        for(i=1;i<=10;i++) {
+            printf("%d x %d = %d\n",n,i, n*i);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    else {
+        printf("PID's P2 - Child of P1: %d\n",childrenPID);
+    }
+    return EXIT_SUCCESS;
+
+}
